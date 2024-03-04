9912a37086159cb63786635563bb759ab1bef046
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2024-03-04 20:21:15 +0300

    filling dynamic array from file

--- Source.c
+++ Source.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <malloc.h>
 
+
+FILE* file = NULL;
 int *userArray;
 int userArrayLen = 0;
+int maxElement = 0;
 
 int main()
 {
-
 	arrayInput();
 	outputUserArray();
 
 	finish();
-	 
-	/*int max = 0;
-
-	for (int i = 0; i < userArrayLength; i++)
-	{
-		userArray[i] = rand() % 5;
-		printf("\t%d", userArray[i]);
-	}
-	printf("\n");
+	
+	/*
 
 	max = userArray[0];
 	for (int i = 0; i < userArrayLength; i++)
@@ -67,31 +62,30 @@ int main()
 
 int arrayInput(void)
 {
-	FILE* file = NULL;
 	char c;
-	int txtArray[128];
 
 	file = fopen("input.txt", "r");
 	if (file == NULL)
 	{
-		printf("File not found");
+		printf("File not found\n");
 		exit(0);
 	}
 
 	while (fscanf(file, "%c", &c) == 1)
 	{
 		fprintf(stdout, "%c", c);
-		userArrayLen++;
+		userArrayLen += 1;
 	}
-	printf("%d", userArrayLen);
+	printf("\n");
+	//printf("\n%d\n", userArrayLen);
 
+	userArrayLen /= 2;
 	userArray = (int*)malloc(userArrayLen * sizeof(int));
+	printf("Original array:\n");
 	for (int i = 0; i < userArrayLen; i++)
 	{
-		fscanf(file, "%d", &userArray[i]);
+		fscanf(file, "%d", &userArray[i]); //why the bunch of fucking fucks???
 	}
-
-	fclose(file);
 }
 
 int outputUserArray(void)
@@ -103,7 +97,13 @@ int outputUserArray(void)
 	printf("\n");
 }
 
+int sortArray(void)
+{
+
+}
+
 int finish(void)
 {
+	fclose(file);
 	free(userArray);
 }
